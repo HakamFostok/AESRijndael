52bfbe4e52d43f022361795a6a2fa0c811858eef
Hakan FISTIK <hakam.fstk@gmail.com>
2021-03-07 17:41:45 +0300

    Fix the code

--- AesRijndaelLibrary/Extensions.cs
+++ AesRijndaelLibrary/Extensions.cs
@@ -7,7 +7,13 @@ namespace AesRijndaelLibrary
     public static class Extensions
     {
         public const int NB = 4;
+        private static Dictionary<char, byte> hexa = new Dictionary<char, byte> { { '0', 0 }, { '1', 1 }, { '2', 2 }, { '3', 3 }, { '4', 4 }, { '5', 5 }, { '6', 6 }, { '7', 7 }, { '8', 8 }, { '9', 9 }, { 'a', 10 }, { 'b', 11 }, { 'c', 12 }, { 'd', 13 }, { 'e', 14 }, { 'f', 15 } };
         private static Dictionary<byte, char> hexarevers;
+
+        static Extensions()
+        {
+            hexarevers = hexa.ToDictionary(c => c.Value, c => c.Key);
+        }
 
         public static string GetHexadecimal(this byte value)
         {
