dcabea231319ae42be4219b4cb8df5db4cfcafd0
Hakan FISTIK <hakam.fstk@gmail.com>
2021-03-07 17:39:03 +0300

    Refactoring GetByteFromHex and unit test it

---
+++ AesRijndael.Test/GetByteFromHexUnitTest.cs
@@ -0,0 +1,58 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AesRijndaelLibrary;
+using System;
+
+namespace AesRijndael.Test
+{
+    [TestClass]
+    public class GetByteFromHexUnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetByteFromHex_NoChar_Fail()
+        {
+            Extensions.GetByteFromHex("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetByteFromHex_3Char_Fail()
+        {
+            Extensions.GetByteFromHex("123");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetByteFromHex_4Char_Fail()
+        {
+            Extensions.GetByteFromHex("1234");
+        }
+
+        [TestMethod]
+        public void GetByteFromHex_Bytes_Success()
+        {
+            for (byte i = 0; i < byte.MaxValue; i++)
+            {
+                string theByteHexValue = BitConverter.ToString(new byte[] { i });
+                
+                var actual1 = Extensions.GetByteFromHex(theByteHexValue);
+                var actual2 = Extensions.GetByteFromHex(theByteHexValue.ToLower());
+
+                Assert.AreEqual(i, actual1);
+                Assert.AreEqual(i, actual2);
+            }
+        }
+
+
+        [TestMethod]
+        public void GetByteFromHex_SingleCharBytes_Success()
+        {
+            for (byte i = 0; i < 10; i++)
+            {
+                var actual = Extensions.GetByteFromHex(i.ToString());
+                Assert.AreEqual(i, actual);
+            }
+        }
+
+    }
+}
--- AesRijndaelLibrary/Extensions.cs
+++ AesRijndaelLibrary/Extensions.cs
@@ -7,13 +7,7 @@ namespace AesRijndaelLibrary
     public static class Extensions
     {
         public const int NB = 4;
-        private static Dictionary<char, byte> hexa = new Dictionary<char, byte> { { '0', 0 }, { '1', 1 }, { '2', 2 }, { '3', 3 }, { '4', 4 }, { '5', 5 }, { '6', 6 }, { '7', 7 }, { '8', 8 }, { '9', 9 }, { 'a', 10 }, { 'b', 11 }, { 'c', 12 }, { 'd', 13 }, { 'e', 14 }, { 'f', 15 } };
         private static Dictionary<byte, char> hexarevers;
-
-        static Extensions()
-        {
-            hexarevers = hexa.ToDictionary(c => c.Value, c => c.Key);
-        }
 
         public static string GetHexadecimal(this byte value)
         {
@@ -32,25 +26,12 @@ namespace AesRijndaelLibrary
             return value;
         }
 
-        public static byte GetByteFromHex(this string name)
+        public static byte GetByteFromHex(this string hexByte)
         {
-            //return name.Length switch
-            //{
-            //    1 => Convert.ToByte(hexa[name[0]]),
-            //    2 => Convert.ToByte(hexa[name[0]] * 16 + hexa[name[1]]),
-            //    _ => throw new ArgumentException("Length must be 1 or 2"),
-            //};
+            if (hexByte.Length is not 1 or 2)
+                throw new ArgumentException("Length must be 1 or 2");
 
-            if (name.Length == 2)
-            {
-                return Convert.ToByte(hexa[name[0]] * 16 + hexa[name[1]]);
-            }
-            if (name.Length == 1)
-            {
-                return Convert.ToByte(hexa[name[0]]);
-            }
-
-            throw new ArgumentException("Length must be 1 or 2");
+            return Convert.ToByte(hexByte, 16);
         }
     }
 }
